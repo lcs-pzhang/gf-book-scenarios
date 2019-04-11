21d84a1236cc133d5f1ac91f41ab35db2443ea9b
Phillip Zhang <pzhang@lcs.on.ca>
2019-04-11 09:44:43 -0400

    Got time tracking working like showing the frame count.

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -9,10 +9,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Piano extends World
 {
     /**
+     * Instance variables
+     * (we can use these anywhere in the class)
+     */
+    int frames;
+    
+    /**
      * Make the piano.
      */
     public Piano() 
     {
         super(800, 340, 1);
+        frames = 0;
+    }
+    
+    /**
+     * act
+     * (gets called repeatedly to create animation)
+     */
+    public void act()
+    {
+        // Track frames
+        frames += 1;
+        //show the frames
+        showText("" + frames, 100, 100);
     }
 }
