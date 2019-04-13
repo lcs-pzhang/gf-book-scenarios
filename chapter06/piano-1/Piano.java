2b45098f14b1984c553a018ff04f3dc4f3659529
Phillip Zhang <pzhang@lcs.on.ca>
2019-04-13 09:59:58 -0400

    Example of how to check two conditions with a Boolean AND operator

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -15,11 +15,9 @@ public class Piano extends World
     int frames;
     // create the array: list
     String[] peopleInClass = {"Julio", "Liam", "Dima", "Simon", "Eric", "Osman", 
-    "Phillip", "Joven", "Nick", "Gordon", "Jieun", "Selena", "Jason", "Sam", 
-    "Joe", "Jeff"};
-    
-    
-    
+            "Phillip", "Joven", "Nick", "Gordon", "Jieun", "Selena", "Jason", "Sam", 
+            "Joe", "Jeff"};
+
     /**
      * Make the piano.
      */
@@ -28,26 +26,24 @@ public class Piano extends World
         super(800, 340, 1);
         frames = 0;
     }
-    
+
     /**
      * act
      * (gets called repeatedly to create animation)
      */
     public void act()
     {
-        //Only show a message when we are in the bounds of the array
-        showText("Array index is: " + frames / 60, 400, 250);
-        
+
         //Only say hello when we are in the bounds of the array
-        if (frames / 60 < 10)
+        if ( (frames % 60 == 0) && (frames / 60 < 10))
         {
-           //Say hello to everyone in class.
-         showText("Hello " + peopleInClass[frames / 60], 400, 170); 
+            //Only show a message when we are in the bounds of the array
+            showText("Array index is: " + frames / 60, 400, 250);
+
+            //Say hello to everyone in class.
+            showText("Hello " + peopleInClass[frames / 60], 400, 170); 
         }
-        
-        
-        
-        
+
         // Track frames
         frames += 1;
         //show the frames
