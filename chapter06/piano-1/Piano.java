1c9447b9c85c501a7106f4aabc29419df8daa2e8
Phillip Zhang <pzhang@lcs.on.ca>
2019-04-16 13:43:14 -0400

    completed the white keys of the piano

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -45,10 +45,18 @@ public class Piano extends World
         //Only say hello when we are in the bounds of the array
         //array
         int position = frames / 60;
-        if ( (frames % 60 == 0) && (position < 15))
+        //Every FULL SECOND when the position is less than count of items in
+        //the array
+        if ( (frames % 60 == 0) && (position < whiteKeys.length))
         {
             //Only show a message when we are in the bounds of the array
             showText("Array index is: " + position, 400, 250);
+            
+            //Create an object to add to the world
+            Key aKey = new Key(whiteKeys[position], whiteNotes[position]);
+            //NOw actually add the object to the world
+            //Object to add, horizontal position, vertical position
+            addObject(aKey, 54 + position * 63, 140);
 
             //Say hello to everyone in class.
             //showText("Hello " + peopleInClass[frames / 60], 400, 170); 
