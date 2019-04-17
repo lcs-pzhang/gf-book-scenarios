1458d4760982a8e79f295bba511501fa2a8e41f4
Phillip Zhang <pzhang@lcs.on.ca>
2019-04-16 20:08:23 -0400

    added more code for the black keys

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -12,20 +12,20 @@ public class Piano extends World
      * Instance variables
      * (we can use these anywhere in the class)
      */
-    int frames;
-    // create the array: This one is for the black keys
-    String[] blackKeys = {"3c#", "3d#", "", "3f#", "3g#", "3a#", 
-            "", "4c#", "4d#", "", "4f#", ""};
-    // This one for the soundfiles for the black keys
-    String[] blackNotes = {"3w", "3e", "", "3t", "3y", "3u", "", "3o", "3p",
-        "", "3]", ""};
-        
+    int frames;    
     //First one traks the keyboard keys that we'll use for white piano keys
     String[] whiteKeys = {"a", "s", "d", "f", "g", "h", "j", "k", "l", ";",
             "'", "\\" };
     //Second array tracks the sound files for each note
     String[] whiteNotes = {"3c", "3d", "3e", "3f", "3g", "3a", "3b", "4c",
             "4d", "4e", "4f", "4g"};
+            
+    // create the array: This one is for the black keys
+    String[] blackKeys = {"3c#", "3d#", "", "3f#", "3g#", "3a#", 
+            "", "4c#", "4d#", "", "4f#", ""};
+    // This one for the soundfiles for the black keys
+    String[] blackNotes = {"3w", "3e", "", "3t", "3y", "3u", "", "3o", "3p",
+        "", "3]", ""};
 
     /**
      * Make the piano.
@@ -54,14 +54,23 @@ public class Piano extends World
             showText("Array index is: " + position, 400, 250);
             
             //Create an object to add to the world
-            Key aKey = new Key(whiteKeys[position], whiteNotes[position]);
+            Key aKey = new aKey(whiteKeys[position], whiteNotes[position]);
             //NOw actually add the object to the world
             //Object to add, horizontal position, vertical position
             addObject(aKey, 54 + position * 63, 140);
-
-            
         }
-
+        
+        if ( (frames % 60 == 0) && (position < blackKeys.length))
+        {
+          //Create an object to add to the world
+            Key aKey = new aKey(blackKeys[position], blackNotes[position]);
+            //NOw actually add the object to the world
+            //Object to add, horizontal position, vertical position
+            addObject(aKey, 85 + position * 63, 86);
+          
+        }
+        
+        
         // Track frames
         frames += 1;
         //show the frames
